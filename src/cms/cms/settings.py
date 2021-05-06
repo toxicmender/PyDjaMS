b1ab03a424f2a449f2d7107850b090dc233c0c1c
toxicmender <13049649+toxicmender@users.noreply.github.com>
2021-05-06 17:42:21 +0530

    chore: Create a path config for serving media

--- src/cms/cms/settings.py
+++ src/cms/cms/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Files uploaded to the CMS for serving
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
