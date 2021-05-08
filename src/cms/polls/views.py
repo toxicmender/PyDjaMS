cf0ee07e217f1b18d91e96e59f193885ef97a041
toxicmender <13049649+toxicmender@users.noreply.github.com>
2021-05-08 23:58:48 +0530

    feat(views): Adds more views for polls app     * Creates a details placeholder view     * Creates a results placeholder view     * Creates a voting placeholder view

--- src/cms/polls/views.py
+++ src/cms/polls/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request: HttpRequest, question_id: int = 0) -> HttpResponse:
+    return HttpResponse(f"You're looking at question: {question_id}")
+
+
+def results(request: HttpRequest, question_id: int = 0) -> HttpResponse:
+    return HttpResponse(f"You're looking at the results of question: {question_id}")
+
+
+def vote(request: HttpRequest, question_id: int = 0) -> HttpResponse:
+    return HttpResponse(f"You're voting on question: {question_id}")
