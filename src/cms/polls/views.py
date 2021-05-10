bd7af8f9604f206cc4b1c3ac697116937082c642
toxicmender <13049649+toxicmender@users.noreply.github.com>
2021-05-10 18:46:27 +0530

    feat(polls): Sorted and paginated result for index
    feat(polls): details now raises 404 when poll id not found

--- src/cms/polls/views.py
+++ src/cms/polls/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse, HttpRequest
+from django.http.response import Http404
+
+from .models import Question
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request: HttpRequest, count: int = 5) -> HttpResponse:
+    latest_questions_list = Question.objects.order_by("-pub_date")[:count]
+    output: str = ", ".join([q.question_test for q in latest_questions_list])
+    return HttpResponse("Hello, world. You're at the polls index.", output)
 
 
 def detail(request: HttpRequest, question_id: int = 0) -> HttpResponse:
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question doesn't exist")
     return HttpResponse(f"You're looking at question: {question_id}")
 
 
